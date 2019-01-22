dc463e58f27e66c9de27f6bcafa2c6a4e97eed75
Pavel Chabinsky <freepk@mail.ru>
2019-01-22 15:38:40 +0300

    Refactor

--- dictionary.go
+++ dictionary.go
@@ -17,12 +17,6 @@ func NewDictionary() *Dictionary {
 }
 
 func (dict *Dictionary) AddToken(value []byte) (int, bool) {
-	dict.RLock()
-	if token, ok := dict.tokens[string(value)]; ok {
-		dict.RUnlock()
-		return token, true
-	}
-	dict.RUnlock()
 	dict.Lock()
 	if token, ok := dict.tokens[string(value)]; ok {
 		dict.Unlock()
