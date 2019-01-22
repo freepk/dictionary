2bf59ade98ee2ce4fca566e853b06cd46881989d
Pavel Chabinsky <freepk@mail.ru>
2019-01-22 13:48:03 +0300

    Refactor

--- dictionary.go
+++ dictionary.go
@@ -2,36 +2,29 @@ package dictionary
 
 import (
 	"sync"
-
-	"github.com/spaolacci/murmur3"
 )
 
 type Dictionary struct {
 	sync.RWMutex
-	tokens map[uint64]int
+	tokens map[string]int
 	values [][]byte
 }
 
 func NewDictionary() *Dictionary {
-	tokens := make(map[uint64]int)
+	tokens := make(map[string]int)
 	values := make([][]byte, 1)
 	return &Dictionary{tokens: tokens, values: values}
 }
 
-func (dict *Dictionary) token(hash uint64) (int, bool) {
-	dict.RLock()
-	token, ok := dict.tokens[hash]
-	dict.RUnlock()
-	return token, ok
-}
-
 func (dict *Dictionary) AddToken(value []byte) (int, bool) {
-	hash := murmur3.Sum64(value)
-	if token, ok := dict.token(hash); ok {
+	dict.RLock()
+	if token, ok := dict.tokens[string(value)]; ok {
+		dict.RUnlock()
 		return token, true
 	}
+	dict.RUnlock()
 	dict.Lock()
-	if token, ok := dict.tokens[hash]; ok {
+	if token, ok := dict.tokens[string(value)]; ok {
 		dict.Unlock()
 		return token, true
 	}
@@ -39,14 +32,16 @@ func (dict *Dictionary) AddToken(value []byte) (int, bool) {
 	temp := make([]byte, len(value))
 	copy(temp, value)
 	dict.values = append(dict.values, temp)
-	dict.tokens[hash] = token
+	dict.tokens[string(value)] = token
 	dict.Unlock()
 	return token, false
 }
 
 func (dict *Dictionary) Token(value []byte) (int, bool) {
-	hash := murmur3.Sum64(value)
-	return dict.token(hash)
+	dict.RLock()
+	token, ok := dict.tokens[string(value)]
+	dict.RUnlock()
+	return token, ok
 }
 
 func (dict *Dictionary) Value(token int) ([]byte, bool) {
